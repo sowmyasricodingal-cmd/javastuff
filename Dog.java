705516b0aa5db171e23bc0ad32c599cc6cc62ff9
sowmyasricodingal-cmd <sowmyasri.codingal@gmail.com>
2026-07-19 15:55:09 +0530

    Create Dog.java

---
+++ Dog.java
@@ -0,0 +1,18 @@
+public class Dog extends Animal {
+
+    private String breed;
+
+    public Dog(String name, int age, String breed) {
+        super(name, age);
+        this.breed = breed;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Woof! Woof!");
+    }
+}
