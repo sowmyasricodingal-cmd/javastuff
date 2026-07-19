f3c75b9bf69d5db0d053ee425e4028a390809a74
sowmyasricodingal-cmd <sowmyasri.codingal@gmail.com>
2026-07-19 14:26:36 +0530

    Create M3A1.java

---
+++ M3A1.java
@@ -0,0 +1,39 @@
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+    this.name = name;
+    this.age = age;
+    }
+
+    public String getName() {
+    return name;
+    }
+
+    public int getAge() {
+    return age;
+    }
+}
+
+public class Student extends Person {
+    private String major;
+
+    public Student(String name, int age, String major) {
+    super(name, age); // Calls Person's constructor
+    this.major = major;
+    }
+
+    public String getMajor() {
+    return major;
+    }
+}
+
+public class M3A1 {
+    public static void main(String[] args) {
+    Student student = new Student("Alice", 20, "Computer Science");
+    System.out.println("Name: " + student.getName());
+    System.out.println("Age: " + student.getAge());
+    System.out.println("Major: " + student.getMajor());
+    }
+}
