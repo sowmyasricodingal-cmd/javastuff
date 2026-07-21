b85567dd89f53dba7052f6cf51fd692f8ebe52d1
sowmyasricodingal-cmd <sowmyasri.codingal@gmail.com>
2026-07-21 14:06:08 +0530

    Create M4l3act1.java

---
+++ M4l3act1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class M4l3act1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int cols = sc.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+
+        System.out.println("Enter matrix elements:");
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Matrix:");
+
+        for (int[] row : matrix) {
+            for (int value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
+    }
+}
